5e82aa2728e43b7ca7f6dbb54211f2f55e8cded2
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-04 00:44:30 +0100

    add updates to use-reducer in cart-reducer

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 
+import CartItem from "./CartItem";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/CartContext";
 
@@ -12,17 +13,29 @@ function Cart(props) {
 
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItems = (
-    <ul className={classes["cart-items"]}>
-      {cartCtx.items.map((item) => (
-        <li key={item.id}>{item.name}</li>
-      ))}
-    </ul>
-  );
+  function cartItemRemoveHandler(id) {}
+  function cartItemAddHandler(item) {}
+
+  const CartItems = () => {
+    return (
+      <ul className={classes["cart-items"]}>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        ))}
+      </ul>
+    );
+  };
 
   return (
     <Modal onClose={props.onCloseCart}>
-      {cartItems}
+      <CartItems />
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
