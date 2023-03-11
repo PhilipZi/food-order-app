24a07f09427bf7617a3fa34e9187516769eaa46f
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-11 22:43:19 +0100

    add functional catch error add error message and css

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,46 +8,55 @@ import Card from "../UI/Card";
 function AvailableMeals() {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMealsHandler = async () => {
       setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(
-          "https://food-order-app-36bf6-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
-        );
-        if (!response.ok) {
-          throw new Error("Something went wrong!");
-        }
+      setHttpError(null);
 
-        const data = await response.json();
-
-        const loadedMeals = [];
-
-        for (const key in data) {
-          loadedMeals.push({
-            id: key,
-            name: data[key].name,
-            description: data[key].description,
-            price: data[key].price,
-          });
-        }
-        console.log(loadedMeals);
-        setMeals(loadedMeals);
-      } catch (error) {
-        setError(error.message);
+      const response = await fetch(
+        "https://food-order-app-36bf6-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
       }
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      console.log(loadedMeals);
+      setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMealsHandler();
+
+    fetchMealsHandler().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.mealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
